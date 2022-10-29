158052fdc35eb1b92c592fa8eacae91779af7d3e
MaTouTch <116964540+MaTouTch@users.noreply.github.com>
2022-10-29 17:07:34 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ README.md
@@ -0,0 +1,31 @@
+
+> Ouvrir cette page à [https://matoutch.github.io/joystick-armando/](https://matoutch.github.io/joystick-armando/)
+
+## Utiliser comme extension
+
+Ce dépôt peut être ajouté en tant qu'**extension** dans MakeCode.
+
+* ouvrir [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* cliquez sur **Nouveau projet**
+* cliquez sur **Extensions** dans le menu engrenage
+* recherchez **https://github.com/matoutch/joystick-armando** et importez
+
+## Éditer ce projet ![Badge du statut de la compilation](https://github.com/matoutch/joystick-armando/workflows/MakeCode/badge.svg)
+
+Éditer ce dépôt dans MakeCode.
+
+* ouvrir [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* cliquez sur **Importer** puis cliquez sur **Importer l'URL **
+* collez **https://github.com/matoutch/joystick-armando** et cliquez sur importer
+
+## Aperçu des blocs
+
+Cette section montre le code des blocs du dernier commit dans la branche master.
+Cette image peut prendre quelques minutes pour être actualisée.
+
+![Un rendu de la vue des blocs](https://github.com/matoutch/joystick-armando/raw/master/.github/makecode/blocks.png)
+
+#### Métadonnées (utilisées pour la recherche, le rendu)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,54 @@
+WSJoyStick.onKey(KEY.F, function () {
+    WSJoyStick.PlayMusic(220, music.beat(BeatFraction.Quarter))
+})
+WSJoyStick.onKey(KEY.A, function () {
+    WSJoyStick.PlayMusic(196, music.beat(BeatFraction.Quarter))
+})
+WSJoyStick.onKey(KEY.E, function () {
+    WSJoyStick.PlayMusic(330, music.beat(BeatFraction.Quarter))
+})
+WSJoyStick.onKey(KEY.D, function () {
+    WSJoyStick.PlayMusic(294, music.beat(BeatFraction.Quarter))
+})
+WSJoyStick.onKey(KEY.B, function () {
+    WSJoyStick.PlayMusic(247, music.beat(BeatFraction.Quarter))
+})
+WSJoyStick.onKey(KEY.C, function () {
+    WSJoyStick.PlayMusic(262, music.beat(BeatFraction.Quarter))
+})
+WSJoyStick.JoyStickInit()
+basic.forever(function () {
+    if (WSJoyStick.Listen_Dir(DIR.U)) {
+        basic.showArrow(ArrowNames.North)
+    } else if (WSJoyStick.Listen_Dir(DIR.D)) {
+        basic.showArrow(ArrowNames.South)
+    } else if (WSJoyStick.Listen_Dir(DIR.L)) {
+        basic.showArrow(ArrowNames.West)
+    } else if (WSJoyStick.Listen_Dir(DIR.R)) {
+        basic.showArrow(ArrowNames.East)
+    } else if (WSJoyStick.Listen_Dir(DIR.U_L)) {
+        basic.showArrow(ArrowNames.NorthWest)
+    } else if (WSJoyStick.Listen_Dir(DIR.U_R)) {
+        basic.showArrow(ArrowNames.NorthEast)
+    } else if (WSJoyStick.Listen_Dir(DIR.D_L)) {
+        basic.showArrow(ArrowNames.SouthWest)
+    } else if (WSJoyStick.Listen_Dir(DIR.D_R)) {
+        basic.showArrow(ArrowNames.SouthEast)
+    } else if (WSJoyStick.Listen_Key(KEY.P)) {
+        basic.showString("P")
+    } else if (WSJoyStick.Listen_Key(KEY.A)) {
+        basic.showString("A")
+    } else if (WSJoyStick.Listen_Key(KEY.B)) {
+        basic.showString("B")
+    } else if (WSJoyStick.Listen_Key(KEY.C)) {
+        basic.showString("C")
+    } else if (WSJoyStick.Listen_Key(KEY.D)) {
+        basic.showString("D")
+    } else if (WSJoyStick.Listen_Key(KEY.E)) {
+        basic.showString("E")
+    } else if (WSJoyStick.Listen_Key(KEY.F)) {
+        basic.showString("F")
+    } else {
+        basic.showIcon(IconNames.Happy)
+    }
+})
