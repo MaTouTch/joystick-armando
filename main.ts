fc07c8995ecb74d7ae18059d4c82895d53e7ee13
MaTouTch <116964540+MaTouTch@users.noreply.github.com>
2022-10-29 18:20:22 +0200

    Messages OK - Plus d'affichage

--- main.ts
+++ main.ts
@@ -27,43 +27,21 @@ radio.setGroup(1)
 radio.sendString("Joystick")
 basic.forever(function () {
     if (WSJoyStick.Listen_Dir(DIR.U)) {
-        radio.sendString("U")
-        basic.showArrow(ArrowNames.North)
+        radio.sendString("N")
     } else if (WSJoyStick.Listen_Dir(DIR.D)) {
-        radio.sendString("D")
-        basic.showArrow(ArrowNames.South)
+        radio.sendString("S")
     } else if (WSJoyStick.Listen_Dir(DIR.L)) {
         radio.sendString("L")
-        basic.showArrow(ArrowNames.West)
     } else if (WSJoyStick.Listen_Dir(DIR.R)) {
         radio.sendString("R")
-        basic.showArrow(ArrowNames.East)
     } else if (WSJoyStick.Listen_Dir(DIR.U_L)) {
-        radio.sendString("UL")
-        basic.showArrow(ArrowNames.NorthWest)
+        radio.sendString("NL")
     } else if (WSJoyStick.Listen_Dir(DIR.U_R)) {
-        radio.sendString("UR")
-        basic.showArrow(ArrowNames.NorthEast)
+        radio.sendString("NR")
     } else if (WSJoyStick.Listen_Dir(DIR.D_L)) {
-        radio.sendString("DL")
-        basic.showArrow(ArrowNames.SouthWest)
+        radio.sendString("SL")
     } else if (WSJoyStick.Listen_Dir(DIR.D_R)) {
-        radio.sendString("DR")
-        basic.showArrow(ArrowNames.SouthEast)
-    } else if (WSJoyStick.Listen_Key(KEY.P)) {
-        basic.showString("P")
-    } else if (WSJoyStick.Listen_Key(KEY.A)) {
-        basic.showString("A")
-    } else if (WSJoyStick.Listen_Key(KEY.B)) {
-        basic.showString("B")
-    } else if (WSJoyStick.Listen_Key(KEY.C)) {
-        basic.showString("C")
-    } else if (WSJoyStick.Listen_Key(KEY.D)) {
-        basic.showString("D")
-    } else if (WSJoyStick.Listen_Key(KEY.E)) {
-        basic.showString("E")
-    } else if (WSJoyStick.Listen_Key(KEY.F)) {
-        basic.showString("F")
+        radio.sendString("SR")
     } else {
         basic.showIcon(IconNames.Happy)
     }
