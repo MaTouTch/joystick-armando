c0bb2555caca3bb7ca919776ce709707666b49ae
MaTouTch <116964540+MaTouTch@users.noreply.github.com>
2022-10-29 18:00:39 +0200

    Bonne version pour faire des tests - Tous les ordres sont transmit - Warn affichage

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 WSJoyStick.onKey(KEY.F, function () {
     WSJoyStick.PlayMusic(220, music.beat(BeatFraction.Quarter))
+    radio.sendString("F")
 })
 WSJoyStick.onKey(KEY.A, function () {
     WSJoyStick.PlayMusic(196, music.beat(BeatFraction.Quarter))
@@ -7,35 +8,47 @@ WSJoyStick.onKey(KEY.A, function () {
 })
 WSJoyStick.onKey(KEY.E, function () {
     WSJoyStick.PlayMusic(330, music.beat(BeatFraction.Quarter))
+    radio.sendString("E")
 })
 WSJoyStick.onKey(KEY.D, function () {
     WSJoyStick.PlayMusic(294, music.beat(BeatFraction.Quarter))
+    radio.sendString("D")
 })
 WSJoyStick.onKey(KEY.B, function () {
     WSJoyStick.PlayMusic(247, music.beat(BeatFraction.Quarter))
+    radio.sendString("B")
 })
 WSJoyStick.onKey(KEY.C, function () {
     WSJoyStick.PlayMusic(262, music.beat(BeatFraction.Quarter))
+    radio.sendString("C")
 })
 WSJoyStick.JoyStickInit()
 radio.setGroup(1)
 radio.sendString("Joystick")
 basic.forever(function () {
     if (WSJoyStick.Listen_Dir(DIR.U)) {
+        radio.sendString("U")
         basic.showArrow(ArrowNames.North)
     } else if (WSJoyStick.Listen_Dir(DIR.D)) {
+        radio.sendString("D")
         basic.showArrow(ArrowNames.South)
     } else if (WSJoyStick.Listen_Dir(DIR.L)) {
+        radio.sendString("L")
         basic.showArrow(ArrowNames.West)
     } else if (WSJoyStick.Listen_Dir(DIR.R)) {
+        radio.sendString("R")
         basic.showArrow(ArrowNames.East)
     } else if (WSJoyStick.Listen_Dir(DIR.U_L)) {
+        radio.sendString("UL")
         basic.showArrow(ArrowNames.NorthWest)
     } else if (WSJoyStick.Listen_Dir(DIR.U_R)) {
+        radio.sendString("UR")
         basic.showArrow(ArrowNames.NorthEast)
     } else if (WSJoyStick.Listen_Dir(DIR.D_L)) {
+        radio.sendString("DL")
         basic.showArrow(ArrowNames.SouthWest)
     } else if (WSJoyStick.Listen_Dir(DIR.D_R)) {
+        radio.sendString("DR")
         basic.showArrow(ArrowNames.SouthEast)
     } else if (WSJoyStick.Listen_Key(KEY.P)) {
         basic.showString("P")
@@ -52,6 +65,6 @@ basic.forever(function () {
     } else if (WSJoyStick.Listen_Key(KEY.F)) {
         basic.showString("F")
     } else {
-        basic.showIcon(IconNames.Heart)
+        basic.showIcon(IconNames.Happy)
     }
 })
