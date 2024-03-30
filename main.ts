1456ac2e0679461b9f9a92a75740d736749b7b6c
MaTouTch <116964540+MaTouTch@users.noreply.github.com>
2024-03-30 14:57:41 +0100

    Ajout de la gestion du display

--- main.ts
+++ main.ts
@@ -1,22 +1,35 @@
 WSJoyStick.onKey(KEY.F, function () {
     WSJoyStick.PlayMusic(220, music.beat(BeatFraction.Quarter))
     radio.sendString("F")
+    _var += -1
+    kitronik_VIEW128x64.show("Score : " + convertToText(_var), 1, kitronik_VIEW128x64.ShowAlign.Centre, kitronik_VIEW128x64.FontSelection.Big)
 })
 WSJoyStick.onKey(KEY.E, function () {
     WSJoyStick.PlayMusic(330, music.beat(BeatFraction.Quarter))
     radio.sendString("E")
+    _var += 5
+    kitronik_VIEW128x64.show("Score : " + convertToText(_var), 1, kitronik_VIEW128x64.ShowAlign.Centre, kitronik_VIEW128x64.FontSelection.Big)
 })
 WSJoyStick.onKey(KEY.D, function () {
     WSJoyStick.PlayMusic(294, music.beat(BeatFraction.Quarter))
     radio.sendString("D")
+    _var += 1
+    kitronik_VIEW128x64.show("Score : " + convertToText(_var), 1, kitronik_VIEW128x64.ShowAlign.Centre, kitronik_VIEW128x64.FontSelection.Big)
 })
 WSJoyStick.onKey(KEY.C, function () {
     WSJoyStick.PlayMusic(262, music.beat(BeatFraction.Quarter))
     radio.sendString("C")
+    _var += -5
+    kitronik_VIEW128x64.show("Score : " + convertToText(_var), 1, kitronik_VIEW128x64.ShowAlign.Centre, kitronik_VIEW128x64.FontSelection.Big)
 })
+let _var = 0
 WSJoyStick.JoyStickInit()
 radio.setGroup(1)
 radio.sendString("Joystick")
+kitronik_VIEW128x64.controlDisplayOnOff(kitronik_VIEW128x64.onOff(true))
+kitronik_VIEW128x64.setFontSize(kitronik_VIEW128x64.FontSelection.Big)
+_var = 20
+kitronik_VIEW128x64.plot(10)
 basic.forever(function () {
     if (WSJoyStick.Listen_Dir(DIR.U)) {
         radio.sendString("N")
@@ -39,6 +52,8 @@ basic.forever(function () {
     } else if (WSJoyStick.Listen_Key(KEY.B)) {
         radio.sendString("B")
     } else {
-    	
+        kitronik_VIEW128x64.show("Compass : " + convertToText(input.compassHeading()), 2, kitronik_VIEW128x64.ShowAlign.Centre, kitronik_VIEW128x64.FontSelection.Big)
+        kitronik_VIEW128x64.show("Compass : " + convertToText(input.lightLevel()), 3, kitronik_VIEW128x64.ShowAlign.Centre, kitronik_VIEW128x64.FontSelection.Big)
+        kitronik_VIEW128x64.plot(input.compassHeading())
     }
 })
